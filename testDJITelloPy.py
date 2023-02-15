da32f80eb992557fdcbd88ecb2d226a12f763852
javacasm <javacasm@gmail.com>
2023-02-15 13:04:33 +0100

    update

--- testDJITelloPy.py
+++ testDJITelloPy.py
@@ -1,9 +1,13 @@
+# instalamos el paquete djitellopy
+
+# pip3 install djitellopy
+
 from djitellopy import tello
 from time import sleep
 me = tello.Tello()
 me.connect(False) # no queremos respuestaa los comandos
 print(me.get_battery())
-me.takeoff()
+me.takeoff()  # despegamos
 me.send_rc_control(0, 50, 0, 0) 
 sleep(2)
 me.send_rc_control(0, 0, 0, 30)
